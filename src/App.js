a8c648f1e7bd2fe7b217d2ae8eb5bd18675c1bf3
DESKTOP-RN3MPFL\siobh <hardtsiobhan@gmail.com>
2023-09-17 01:17:26 +0100

    path change again

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ function App() {
         <Routes>
           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
           <Route path="/terms-of-service" element={<TermsOfService />} />
-          <Route path="/3dprinting" element={
+          <Route path="/" element={
             <>
       <section id="home" className="full-page-header">
         <img className="lefthex" src={lefthex} alt="Left Hex" />
